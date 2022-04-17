3f6535adad48e2794215e25382f49d6578d996b4
peter-siebold <peter.siebold@icloud.com>
2022-04-17 07:53:00 +0200

    Set up CORS for test server and configure sass

--- server.js
+++ server.js
@@ -1,32 +1,23 @@
 const fs = require('fs');
+const cors = require('cors');
 const express = require('express');
 const app = express();
+
+
+app.use(cors());
+app.options('*', cors());
 
 app.get('/stations', (req, res) => {
     fs.readFile('./src/api/stations.json', 'utf8', (err, data) => {
         if (err) {
             res.status(500).send(err);
         } else {
-            res.send(data);
+            setTimeout(() => {
+                res.send(data);
+            }, 100);
         }
     });
 });
-
-// app.get('/stations/:id', (req, res) => {
-//     fs.readFile('./src/api/stations.json', 'utf8', (err, data) => {
-//         if (err) {
-//             res.status(500).send(err);
-//         } else {
-//             const stations = JSON.parse(data);
-//             const station = stations.find(station => station.id === req.params.id);
-//             if (station) {
-//                 res.send(station);
-//             } else {
-//                 res.status(404).send('Station not found');
-//             }
-//         }
-//     });
-// });
 
 app.get('/stations/:stationId/bookings/:bookingId', (req, res) => {
     fs.readFile('./src/api/stations.json', 'utf8', (err, data) => {
@@ -49,8 +40,8 @@ app.get('/stations/:stationId/bookings/:bookingId', (req, res) => {
     });
 });
 
+const PORT = 3003;
 
-
-app.listen(3000, () => {
-    console.log('Example app listening on port 3000!');
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}!`);
 });
